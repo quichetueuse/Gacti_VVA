b3c4544296ce9211398aac5c733ad8c2d5991a97
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-01-05 10:14:45 +0100

    - Models are now child of BaseModel.php

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -1,8 +1,29 @@
 <?php
-
 namespace Models;
+require_once('../autoloader.php');
+
+use PDO;
 
 class BaseModel
 {
+    private PDO $pdoClient;
+    private $DbManager;
+    public function __construct(){
 
+        //creating connection object
+        $this->DbManager = new Db('localhost', 'gacti', 'root', '');
+        $this->pdoClient = $this->DbManager->getPdoClient();
+    }
+
+
+//    protected function selectAll(): array {
+//
+//        return array();
+//    }
+//
+//
+//
+//    protected function countAll(): int () {
+//
+//}
 }
