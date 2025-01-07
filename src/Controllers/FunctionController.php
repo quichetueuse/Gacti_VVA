195113bb3360e61abe72d1512ae1f8fc46fdd0dc
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-01-07 10:42:32 +0100

    - Added styling on some elements - Moved inline styling to file styling on some elements - Title is now centered

--- src/Controllers/FunctionController.php
+++ src/Controllers/FunctionController.php
@@ -27,8 +27,8 @@ final class FunctionController extends BaseController
         $is_user_connected = array_key_exists('prenom', $_SESSION);
 
         $header_div = '
-                <div class="header-div" style="border: 1px solid black; justify-content: space-between">
-                    <div class="flex-column-center" style="border: 1px solid blue;">
+                <div class="header-div">
+                    <div class="flex-column-center">
                         <!-- session info div-->
                         <div>
                             <span><b>'.  $nom . ' ' . $prenom . '</b></span>
@@ -45,7 +45,7 @@ final class FunctionController extends BaseController
                             </script>
                         </div>
                     </div>
-                    <h1 style="height: 100%; display: flex; align-items: center; justify-content: center;">VVA</h1>
+                    <h1 class="page-h1">VVA</h1>
             ';
 
 
@@ -53,7 +53,7 @@ final class FunctionController extends BaseController
         {
             $header_div .= '
                 <!-- disconnect button div-->
-                <div class="flex-center" style="border: 1px solid blue;">
+                <div class="flex-center">
                     <a class="user-logoff-button" onclick="showConfirmDisconnect();" title="Se déconnecter">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="32px" height="32px"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V256c0 17.7 14.3 32 32 32s32-14.3 32-32V32zM143.5 120.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6c-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4c0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z"/></svg>
                     </a>
@@ -261,11 +261,11 @@ final class FunctionController extends BaseController
             $total_user_inscription = $this->inscription_controller->getCountInscriptionByUser($user['NOMCOMPTE'], $user['PRENOMCOMPTE']);
             $user_card = '
                 <div class="user-summary-card">
-                    <h3 class="card-title text-center grey-border bold" style="border-bottom: 1px solid white; padding-bottom: 1rem;">Mr '. $user["NOMCOMPTE"] . $user["PRENOMCOMPTE"] .'</h3>
-                    <p style="padding-top: 1rem;"><strong>En vacances du <span style="font-size: 15pt; color: palegreen" class=" grey-border">'. $user["DATEDEBSEJOUR"] .'</span> au <span style="font-size: 15pt; color: palegreen" class=" grey-border">'. $user["DATEFINSEJOUR"] . '</span></strong></p>
-                    <p><strong>Date d\'inscription:&nbsp<span style="font-size: 15pt; color: palegreen" class=" grey-border">'. $user["DATEINSCRIP"] .'</span></strong></p>
-                    <p><strong>Code de l\'inscription:&nbsp<span style="font-size: 15pt; color: palegreen" class=" grey-border">'. $user["NOINSCRIP"] .'</span></strong></p>
-                    <p><strong>Inscrit à<span style="font-size: 15pt; color: palegreen" class=" grey-border">&nbsp'. $total_user_inscription .'&nbsp</span>activités au total</strong></p>
+                    <h3 class="card-title text-center grey-border bold insc-card-title">Mr '. $user["NOMCOMPTE"] . $user["PRENOMCOMPTE"] .'</h3>
+                    <p style="padding-top: 1rem;"><strong>En vacances du <span class="visible-value grey-border">'. $user["DATEDEBSEJOUR"] .'</span> au <span style="font-size: 15pt; color: palegreen" class=" grey-border">'. $user["DATEFINSEJOUR"] . '</span></strong></p>
+                    <p><strong>Date d\'inscription:&nbsp<span class="visible-value grey-border">'. $user["DATEINSCRIP"] .'</span></strong></p>
+                    <p><strong>Code de l\'inscription:&nbsp<span class="visible-value grey-border">'. $user["NOINSCRIP"] .'</span></strong></p>
+                    <p><strong>Inscrit à<span class="visible-value grey-border">&nbsp'. $total_user_inscription .'&nbsp</span>activités au total</strong></p>
                 </div>
             ';
 
