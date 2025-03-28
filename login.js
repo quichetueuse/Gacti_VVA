c6123285493107b6c29eb308a732002491fe316c
quichetueuse <144886326+quichetueuse@users.noreply.github.com>
2025-03-28 22:19:29 +0100

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-function view_password_v2(element_id) {
-    var input_p = document.getElementById(element_id);
-    if (input_p.type === "password") {
-        input_p.type = "text";
-    } else {
-        input_p.type = "password";
-    }
-}
