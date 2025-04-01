e31b4f4b23b2ea02b610ca700a847a8e9094c734
Eliot Barrabes <eliot.barrabes@gmail.com>
2025-04-01 10:52:09 +0200

    - Removed unused commented code

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,11 +1,7 @@
 <?php
 
 namespace Models;
-//require_once ("../_config.inc");
 require_once('../autoloader.php');
-//require('Db.php');
-//use Models\Db;
-//use PDO;
 use Models\Db;
 use PDO;
 
@@ -18,19 +14,8 @@ class User extends BaseModel
 
     public function verify_credentials(string $email, string $password){
         $sqlQuery = 'SELECT USER, MDP, NOMCOMPTE, PRENOMCOMPTE, DATEINSCRIP, DATEFERME, TYPEPROFIL, DATEDEBSEJOUR, DATEFINSEJOUR, DATENAISCOMPTE, ADRMAILCOMPTE, NOTELCOMPTE FROM compte WHERE ADRMAILCOMPTE=:email AND MDP=:mdp';
-//        $this->pdoClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//        $this->pdoClient->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $loginStatement = $this->pdoClient->prepare($sqlQuery);
         $loginStatement->execute(['email' => $email, 'mdp' => $password]);
-//        if($loginStatement->rowCount() == 0)
-//        {
-//            // echo 'nothing found';
-////            header("location: " . SITE_URL . "/Login/login_failed.php");
-//            echo 'FAILED';
-//            return;
-//        }
-
-//        echo "================" . $actionStatement->fetch()['USER'];
         return $loginStatement->fetch();
     }
 
@@ -48,9 +33,6 @@ class User extends BaseModel
         $sqlQuery = 'SELECT NOMCOMPTE, PRENOMCOMPTE FROM compte WHERE USER=:user_id';
         $userStatement = $this->pdoClient->prepare($sqlQuery);
         $userStatement->execute(['user_id' => $user_id]);
-//        if ($userStatement->fetch(PDO::FETCH_ASSOC) === false) {
-//            return array('NOMCOMPTE' => '', 'PRENOMCOMPTE' => '');
-//        }
         //si aucun utilisateur n'est trouvé
         if ($userStatement->rowCount() == 0) {
             return false;
